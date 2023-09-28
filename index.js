15fe327ffb3489cdfff606cfd72b7e4004dfd429
Earl <earlmunyua96@gmail.com>
2023-09-29 00:34:44 +0300

    Java script Loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = "surprise"
+
+
+const thankYouMessage = []
+function writeCards(names, event) {
+for (let i=0; i < names.length; i++){
+const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+thankYouMessage.push(message)   
+}
+return thankYouMessage
+}
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
